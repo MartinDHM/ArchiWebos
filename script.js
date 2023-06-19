61638a6440449f1946d7677a33c6cdf0ef1a9ac9
MartinDHM <martin.duhem1@gmail.com>
2023-06-19 09:27:50 +0200

    Ajout de la page de login et resize des 2 pages pour mieux correspondre a la maquette

--- script.js
+++ script.js
@@ -1,76 +1,7 @@
-// /* recupération des données par l'api */
-// fetch('http://localhost:5678/api/works')
-//   .then(response => response.json())
-//   .then(data => {
-//     afficherGallery(data);
-//     console.log(data[0]);
-//   })
-
-// /* fonction qui permet affiche la gallery*/
-// function afficherGallery(data){
-//   for(i =0 ; i < data.length ; i++)
-//   {
-//     const gallery = document.querySelector('.gallery');
-
-//     const figure  = document.createElement("figure");
-
-//     const imageElement = document.createElement("img");
-//     imageElement.src = data[i].imageUrl;
-
-//     const nomElement = document.createElement("p");
-//     nomElement.innerText = data[i].title;
-
-//     gallery.appendChild(figure)
-//     figure.appendChild(imageElement)
-//     figure.appendChild(nomElement)
-//   }
-// }
-// /*recupération des catégorie des filtre de l'api */
-// fetch('http://localhost:5678/api/categories')
-//     .then(response => response.json())
-//     .then(category => {
-//       filtersBtn(category);
-//       console.log(category[i]);
-//     })
-//     .catch(error => {
-//       console.error('Une erreur s\'est produite', error);
-//     });
-// function filters (category){
-
-//   category.unshift({ name: "Tous" });
-
-//   for(i = 0; i < category.length; i++)
-//   {
-//     const filterbtn = document.querySelector(".filtersBtn")
-//     const filter = document.createElement("button");
-//     filter.innerText = category[i].name;
-
-//     filterbtn.appendChild(filter);
-//   }
-
-// const filterBtnEvent = document.querySelectorAll(".filterBtn button");
-
-// filterBtnEvent.forEach(button => {
-//   button.addEventListener("click", function() {
-//     // Efface le contenu de la galerie
-//     document.querySelector(".gallery").innerHTML = '';
-
-//     // Filtrage des projets en fonction de la catégorie sélectionnée
-//     let filteredData = data.filter((item) => {
-//       return item.category.name === button.textContent || button.textContent === "Tous";
-//     });
-
-//     console.log(filteredData);
-
-//     // Utilisation des projets filtrés pour afficher la galerie mise à jour
-//     genererWorks(filteredData);
-//   });
-// });
-// }
 
 let worksData, categoriesData;
 
-/* recupération des données par l'api */
+/* recupération des données de l'Api */
 fetch('http://localhost:5678/api/works')
   .then(response => response.json())
   .then(data => {
@@ -85,9 +16,7 @@ fetch('http://localhost:5678/api/works')
     filters(category);
     console.log(category[0]);
   })
-  .catch(error => {
-    console.error('Une erreur s\'est produite', error);
-  });
+ 
 
 /* fonction qui permet affiche la gallery */
 function afficherGallery(data){
@@ -95,7 +24,7 @@ function afficherGallery(data){
   {
     const gallery = document.querySelector('.gallery');
 
-    const figure  = document.createElement("figure");
+    const figures  = document.createElement("figure");
 
     const imageElement = document.createElement("img");
     imageElement.src = data[i].imageUrl;
@@ -103,12 +32,12 @@ function afficherGallery(data){
     const nomElement = document.createElement("p");
     nomElement.innerText = data[i].title;
 
-    gallery.appendChild(figure);
-    figure.appendChild(imageElement);
-    figure.appendChild(nomElement);
+    gallery.appendChild(figures);
+    figures.appendChild(imageElement);
+    figures.appendChild(nomElement);
   }
 }
-
+/* fonction qui affiche les filtres */
 function filters (category){
   category.unshift({ name: "Tous" });
 
@@ -121,6 +50,7 @@ function filters (category){
     filterbtn.appendChild(filter);
   }
 
+  /* Ajout des evenements sur les filtres */
   const filterBtnEvent = document.querySelectorAll(".filtersBtn button");
 
   filterBtnEvent.forEach(button => {

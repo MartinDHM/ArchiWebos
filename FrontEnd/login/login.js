b59e067e36224a922082fec8141429804499a725
MartinDHM <martin.duhem1@gmail.com>
2023-06-23 17:24:14 +0200

    ajout du stockage du token et des condition de connexion

---
+++ FrontEnd/login/login.js
@@ -0,0 +1,43 @@
+async function login() {
+  try {
+    // On récupère les données à envoyer
+    const loginForm = document.querySelector(".login-in");
+    const error = document.querySelector(".error");
+    loginForm.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const logData = {
+        email: event.target.querySelector("[name=email]").value,
+        password: event.target.querySelector("[name=motdepasse]").value,
+      };
+      try {
+        const response = await fetch("http://localhost:5678/api/users/login", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(logData),
+        });
+
+        // On vérifie si la requête a réussi
+        if (response.ok) {
+          const token = await response.json();
+          // On stocke le token récupéré dans le sessionStorage
+          sessionStorage.setItem("token", JSON.stringify(token));
+          // On redirige vers la page d'accueil
+          window.location.href = "../index.html";
+        } else {
+          // Afficher un message d'erreur en cas d'échec
+          error.textContent = "Nom d'utilisateur ou mot de passe incorrect";
+        }
+      } catch (error) {
+        // Afficher une alerte en cas de problème de connexion au serveur
+        alert("Problème de connexion au serveur");
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+login();

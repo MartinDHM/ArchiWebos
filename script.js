d834cb9dc5764ad97181027c122f9d7004b2a3f3
MartinDHM <martin.duhem1@gmail.com>
2023-06-30 08:31:25 +0200

    mise en place du formulaire pour ajouté des photo

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ fetch('http://localhost:5678/api/works')
 function afficherGallery(data) {
   const gallery = document.querySelector('.gallery');
 
+  gallery.innerHTML = ''; // Effacer le contenu existant de la galerie
+
   for (let i = 0; i < data.length; i++) {
     const figure = document.createElement("figure");
     figure.id = `image-${data[i].id}`;
@@ -41,6 +43,7 @@ function filters(category) {
   category.unshift({ name: "Tous" });
 
   const filterBtn = document.querySelector(".filtersBtn");
+  filterBtn.innerHTML = ''; // Effacer le contenu existant des filtres
 
   for (let i = 0; i < category.length; i++) {
     const filter = document.createElement("button");
@@ -135,79 +138,212 @@ let cross = null; // Variable pour stocker la référence à la croix
 
 // Lorsque l'on appuie sur le bouton "modifier", on affiche le modal
 openModalLink.addEventListener("click", () => {
-  if (!galleryAdded) { // Vérifier si la galerie n'a pas déjà été ajoutée
-
-    cross = document.createElement("i");
-    cross.className = "cross fas fa-times";
-
-    cross.addEventListener("click", (event) => {
-      event.stopPropagation(); // Empêcher la propagation de l'événement de clic
-
-      const closeModalLink = document.getElementById('modalId');
-      closeModalLink.classList.add("modalVisibility");
-    });
-
-    const modalAddTitle = document.createElement("h3");
-    modalAddTitle.textContent = "Galerie photo";
-    const modalPhotoCtn = document.createElement("div");
-    modalPhotoCtn.classList.add("photo-ctn");
-    const modalBorder = document.createElement("div");
-    modalBorder.classList.add("border");
-    const modalAddButton = document.createElement("button");
-    modalAddButton.classList.add("addButton");
-    modalAddButton.textContent = "Ajouter une photo";
-    const modalRemoveButton = document.createElement("button");
-    modalRemoveButton.classList.add("removeButton");
-    modalRemoveButton.textContent = "Supprimer la galerie";
-
-    modalWrapper.appendChild(cross);
-    modalWrapper.appendChild(modalAddTitle);
-    modalWrapper.appendChild(modalPhotoCtn);
-    modalWrapper.appendChild(modalBorder);
-    modalWrapper.appendChild(modalAddButton);
-    modalWrapper.appendChild(modalRemoveButton);
-
-    // Ajouter les "works" dans la galerie du modal
-    for (let i = 0; i < worksData.length; i++) {
-      const modalPhoto = document.createElement("div");
-      modalPhoto.classList.add("modal-photo");
-
-      const modalPhotoImage = document.createElement("img");
-      modalPhotoImage.src = worksData[i].imageUrl;
-      modalPhotoImage.classList.add("modal-image");
-
-      const modalTrashIcon = document.createElement("i"); // Nouvel élément pour l'icône de corbeille
-      modalTrashIcon.classList.add("modal-trash-icon"); // Ajoutez une classe pour styliser l'icône
-      modalTrashIcon.classList.add("fa-solid", "fa-trash-can"); // Ajoutez des classes supplémentaires pour utiliser une icône de la bibliothèque Font Awesome (facultatif)
-
-      const modalPhotoTitle = document.createElement("h4");
-      modalPhotoTitle.textContent = worksData[i].title;
-      modalPhotoTitle.classList.add("modal-title");
-
-      modalPhoto.appendChild(modalPhotoImage);
-      modalPhoto.appendChild(modalTrashIcon); // Ajoutez l'icône de corbeille après l'image
-      modalPhoto.appendChild(modalPhotoTitle);
-      modalPhotoCtn.appendChild(modalPhoto);
-
-      modalTrashIcon.addEventListener("click", () => {
-        const imageId = worksData[i].id; // Récupérer l'ID de l'image
-        const galleryImages = document.querySelectorAll(".gallery figure"); // Sélectionner toutes les figures/images de la galerie
-
-        // Parcourir toutes les images et supprimer celle avec l'ID correspondant
-        galleryImages.forEach((image) => {
-          if (image.id === `image-${imageId}`) {
-            image.remove(); // Supprimer l'image de la galerie
-          }
-        });
-
-        // Supprimer également l'image du modal
-        modalPhoto.remove();
+  
+  function createModal() {
+    if (!galleryAdded) { // Vérifier si la galerie n'a pas déjà été ajoutée
+      cross = document.createElement("i");
+      cross.className = "cross fas fa-times";
+  
+      cross.addEventListener("click", (event) => {
+        event.stopPropagation(); // Empêcher la propagation de l'événement de clic
+  
+        const closeModalLink = document.getElementById('modalId');
+        closeModalLink.classList.add("modalVisibility");
       });
+  
+      const modalAddTitle = document.createElement("h3");
+      modalAddTitle.textContent = "Galerie photo";
+      const modalPhotoCtn = document.createElement("div");
+      modalPhotoCtn.classList.add("photo-ctn");
+      const modalBorder = document.createElement("div");
+      modalBorder.classList.add("border");
+      const modalAddButton = document.createElement("button");
+      modalAddButton.classList.add("addButton");
+      modalAddButton.textContent = "Ajouter une photo";
+      const modalRemoveButton = document.createElement("button");
+      modalRemoveButton.classList.add("removeButton");
+      modalRemoveButton.textContent = "Supprimer la galerie";
+  
+      modalWrapper.appendChild(cross);
+      modalWrapper.appendChild(modalAddTitle);
+      modalWrapper.appendChild(modalPhotoCtn);
+      modalWrapper.appendChild(modalBorder);
+      modalWrapper.appendChild(modalAddButton);
+      modalWrapper.appendChild(modalRemoveButton);
+  
+      // Ajouter les "works" dans la galerie du modal
+      for (let i = 0; i < worksData.length; i++) {
+        const modalPhoto = document.createElement("div");
+        modalPhoto.classList.add("modal-photo");
+        modalPhoto.id = `image-${worksData[i].id}`; // Ajouter un ID unique à chaque image
+  
+        const modalPhotoImage = document.createElement("img");
+        modalPhotoImage.src = worksData[i].imageUrl;
+        modalPhotoImage.classList.add("modal-image");
+  
+        const modalTrashIcon = document.createElement("i"); // Nouvel élément pour l'icône de corbeille
+        modalTrashIcon.classList.add("modal-trash-icon"); // Ajoutez une classe pour styliser l'icône
+        modalTrashIcon.classList.add("fa-solid", "fa-trash-can"); // Ajoutez des classes supplémentaires pour utiliser une icône de la bibliothèque Font Awesome (facultatif)
+  
+        const modalUpDownLeftRightIcon = document.createElement("i"); // Nouvel élément pour l'icône up-down-left-right
+        modalUpDownLeftRightIcon.classList.add("modal-up-down-left-right-icon"); // Ajoutez une classe pour styliser l'icône
+        modalUpDownLeftRightIcon.classList.add("fa-solid", "fa-up-down-left-right"); // Ajoutez des classes supplémentaires pour utiliser une icône de la bibliothèque Font Awesome
+  
+        const modalPhotoTitle = document.createElement("h4");
+        modalPhotoTitle.textContent = worksData[i].title;
+        modalPhotoTitle.classList.add("modal-title");
+  
+        modalPhoto.appendChild(modalPhotoImage);
+        modalPhoto.appendChild(modalTrashIcon); // Ajoutez l'icône de corbeille après l'image
+        modalPhoto.appendChild(modalUpDownLeftRightIcon); // Ajoutez l'icône up-down-left-right après l'icône de corbeille
+        modalPhoto.appendChild(modalPhotoTitle);
+        modalPhotoCtn.appendChild(modalPhoto);
+  
+        modalTrashIcon.addEventListener("click", () => {
+          const imageId = worksData[i].id; // Récupérer l'ID de l'image
+          const galleryImages = document.querySelectorAll(".modal-photo"); // Sélectionner toutes les figures/images de la galerie
+  
+          // Parcourir toutes les images et supprimer celle avec l'ID correspondant
+          galleryImages.forEach((image) => {
+            if (image.id === `image-${imageId}`) {
+              image.remove(); // Supprimer l'image de la galerie
+            }
+          });
+  
+          // Supprimer également l'image du modal
+          modalPhoto.remove();
+        });
+      }
+  
+      galleryAdded = true; // Marquer la galerie comme ajoutée
     }
-
-    galleryAdded = true; // Marquer la galerie comme ajoutée
+  
+    const modalVisibility = document.getElementById('modalId');
+    modalVisibility.classList.remove("modalVisibility");
   }
+  
+  createModal();
+  
+  // Ajout du modal pour ajouter des photos
+  const modalAddButton = document.querySelector(".addButton");
+  modalAddButton.addEventListener("click", () => {
+  modalWrapper.innerHTML = ""; // Effacer le contenu du wrapper
 
-  const modalVisibility = document.getElementById('modalId');
-  modalVisibility.classList.remove("modalVisibility");
+  const closeButton = document.createElement("i");
+  closeButton.classList.add("fa-solid", "fa-times", "close-button");
+
+  const ajoutPhoto = document.createElement("h3");
+  ajoutPhoto.classList.add("ajoutPhoto");
+  ajoutPhoto.innerHTML = "Ajout photo";
+
+  const iconeBack = document.createElement("i");
+  iconeBack.classList.add("fa-solid", "fa-arrow-left");
+
+  const addForm = document.createElement("form");
+  addForm.classList.add("addForm");
+  const addBox = document.createElement("div");
+  addBox.classList.add("addBox");
+  const iconeImg = document.createElement("i");
+  iconeImg.classList.add("fa-regular", "fa-image");
+  const addImgButton = document.createElement("button");
+  addImgButton.innerText = "+ Ajouter photo";
+  addImgButton.classList.add("addImgButton");
+  const inputFileBtn = document.createElement("input");
+  inputFileBtn.type = "file";
+  inputFileBtn.accept = ".jpg, .png";
+  inputFileBtn.classList.add("inputFileBtn");
+  const photoPreview = document.createElement("img");
+  photoPreview.id = "photoPreview";
+  const addImgText = document.createElement("p");
+  addImgText.innerText = "jpg, png : 4mo max";
+  const inputCtn = document.createElement("div");
+  inputCtn.classList.add("inputCtn");
+  const labelInputTitle = document.createElement("label");
+  labelInputTitle.setAttribute("for", "Title");
+  labelInputTitle.innerText = "Titre";
+  const inputTitle = document.createElement("input");
+  inputTitle.id = "Title";
+  inputTitle.type = "text";
+  const labelSelectCategory = document.createElement("label");
+  labelSelectCategory.setAttribute("for", "category");
+  labelSelectCategory.innerText = "Catégorie";
+  const selectCategory = document.createElement("select");
+  selectCategory.classList.add("optionCategorie");
+  selectCategory.id = "category";
+
+  const option1 = document.createElement("option");
+  option1.value = "option1Value";
+  option1.text = "Choissisez une catégorie :";
+
+  const option2 = document.createElement("option");
+  option2.value = "option2Value";
+  option2.text = "Objets";
+
+  const option3 = document.createElement("option");
+  option3.value = "option3Value";
+  option3.text = "Appartements";
+
+  const option4 = document.createElement("option");
+  option4.value = "option4Value";
+  option4.text = "Hotels & restaurants";
+
+  selectCategory.appendChild(option1);
+  selectCategory.appendChild(option2);
+  selectCategory.appendChild(option3);
+  selectCategory.appendChild(option4);
+
+  const border = document.createElement("div");
+  border.classList.add("border");
+  const formSubmitButton = document.createElement("input");
+  formSubmitButton.classList.add("formSubmitButton");
+  formSubmitButton.type = "submit";
+  formSubmitButton.value = "Valider";
+  formSubmitButton.disabled = true;
+
+  addForm.appendChild(addBox);
+  addBox.appendChild(iconeImg);
+  addBox.appendChild(addImgButton);
+  addBox.appendChild(inputFileBtn);
+  addBox.appendChild(photoPreview);
+  addBox.appendChild(addImgText);
+  addForm.appendChild(inputCtn);
+  inputCtn.appendChild(labelInputTitle);
+  inputCtn.appendChild(inputTitle);
+  inputCtn.appendChild(labelSelectCategory);
+  inputCtn.appendChild(selectCategory);
+  addForm.appendChild(border);
+  addForm.appendChild(formSubmitButton);
+
+  modalWrapper.appendChild(closeButton);
+  modalWrapper.appendChild(ajoutPhoto);
+  modalWrapper.appendChild(iconeBack);
+  modalWrapper.appendChild(addForm);
+
+  // Événement de changement de fichier
+  inputFileBtn.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      photoPreview.src = e.target.result;
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  });
+
+
+  closeButton.addEventListener("click", (event) => {
+    event.stopPropagation(); // Empêcher la propagation de l'événement de clic
+
+    const closeModalLink = document.getElementById("modalId");
+    closeModalLink.classList.add("modalVisibility");
+
+    
+  })
+  });
 });
+
+
+
